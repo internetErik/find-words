a4a0cec9ae4bd2fbcb106aae67ef898d6304e592
Erik Christianson <internet.erik@gmail.com>
2021-11-27 10:07:50 -0600

    rewriting how commands are processed, and adding case sensitive and recursive options

--- index.js
+++ index.js
@@ -3,77 +3,63 @@ const path = require('path');
 
 let args = process.argv.slice(2);
 
-let directory = './';
-let words = [];
-let extensions = [];
-let ignores = [
+const argProcessorFactory = (short, long, processor) => args => (
+  (args.includes(short)) ? processor(args, short)
+: (args.includes(long))  ? processor(args, long)
+: null
+)
+
+const genericSingleArgProcessor = (args, flag) => {
+  const index = args.indexOf(flag);
+  const result = args[index + 1];
+  args.splice(index, 2);
+  return result;
+}
+
+const genericMultiArgProcessor = (args, flag) => {
+  const index = args.indexOf(flag);
+  const endIndex = args.slice(index + 1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
+  const result = args.slice(index + 1, endIndex === -1 ? args.length : endIndex + 1);
+  args.splice(index, result.length + 1);
+  return result;
+}
+
+const processDirectory = argProcessorFactory('-d', '--directory', genericSingleArgProcessor)
+const processWords = argProcessorFactory('-w', '--words', genericMultiArgProcessor)
+const processExtensions = argProcessorFactory('-e', '--extensions', genericMultiArgProcessor)
+const processIgnores = argProcessorFactory('-i', '--ignores', genericMultiArgProcessor)
+const processRecursive = argProcessorFactory('-r', '--recursive', (args, flag) => true)
+const processCaseSensitive = argProcessorFactory('-c', '--case-sensitive', (args, flag) => true)
+const defaultDirectory = './';
+const defaultWords = [];
+const defaultExtensions = [];
+const defaultIgnores = [
   'node_modules',
   '.git',
 ];
+const defaultRecursive = false;
+const defaultCaseSensitive = false;
 
 const data = [];
 
-console.log(args);
 // get directory out of args
-if(args.includes('-d')) {
-  let index = args.indexOf('-d');
-  directory = args[index+1];
-  args.splice(index, 2);
-}
-if(args.includes('--directory')) {
-  let index = args.indexOf('--directory');
-  directory = args[index+1];
-  args.splice(index, 2);
-}
-
-if(args.includes('-w')) {
-  let index = args.indexOf('-w');
-  let endIndex = args.slice(index+1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
-  console.log(endIndex);
-  words = args.slice(index+1, endIndex === -1 ? args.length : endIndex+1);
-  args.splice(index, words.length+1);
-}
-if(args.includes('--words')) {
-  let index = args.indexOf('--words');
-  let endIndex = args.slice(index+1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
-  words = args.slice(index+1, endIndex === -1 ? args.length : endIndex+1);
-  args.splice(index, words.length+1);
-}
-
-if(args.includes('-e')) {
-  let index = args.indexOf('-e');
-  let endIndex = args.slice(index+1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
-  extensions = args.slice(index+1, endIndex === -1 ? args.length : endIndex+1);
-  args.splice(index, extensions.length+1);
-}
-if(args.includes('--extensions')) {
-  let index = args.indexOf('--extensions');
-  let endIndex = args.slice(index+1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
-  extensions = args.slice(index+1, endIndex === -1 ? args.length : endIndex+1);
-  args.splice(index, extensions.length+1);
-}
-
-if(args.includes('-i')) {
-  let index = args.indexOf('-i');
-  let endIndex = args.slice(index+1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
-  ignores = args.slice(index+1, endIndex === -1 ? args.length : endIndex+1);
-  args.splice(index, ignores.length+1);
-}
-if(args.includes('--ignores')) {
-  let index = args.indexOf('--ignores');
-  let endIndex = args.slice(index+1).reduce((acc, arg, i) => acc !== -1 ? acc : (arg.indexOf('-') === 0 ? i : -1), -1)
-  ignores = args.slice(index+1, endIndex === -1 ? args.length : endIndex+1);
-  args.splice(index, ignores.length+1);
-}
-
-// console.log('post args', args);
-console.log('searching:', directory);
-console.log('only checking files with extensions:', extensions.length > 0 ? extensions : 'all');
-console.log('looking for words:', words)
-console.log('ignoring directories:', ignores.length > 0 ? ignores : 'none');
+let directory = processDirectory(args) || defaultDirectory;
+let words = processWords(args) || defaultWords;
+const extensions = processExtensions(args) || defaultExtensions;
+const ignores = processIgnores(args) || defaultIgnores;
+const recursive = processRecursive(args) || defaultRecursive;
+const caseSensitive = processCaseSensitive(args) || defaultCaseSensitive;
 
 // make sure there is a / at the end of the directory
-if(directory && directory[directory.length - 1] !== '/') directory += '/';
+if (directory && directory[directory.length - 1] !== '/') directory += '/';
+
+if(caseSensitive)
+  words = words.map(word => word.toLowerCase());
+
+console.log('searching:', directory, `(${ recursive ? '' : 'not'} recursively)`);
+console.log('only checking files with extensions:', extensions.length > 0 ? extensions : 'all');
+console.log('looking for words:', words, `(Case-${ caseSensitive ? 'S' : 'Ins' }ensitive)`)
+console.log('ignoring directories:', ignores.length > 0 ? ignores : 'none');
 
 const readFiles = (dirname, onFileContent, onError) => new Promise((resolve, reject) => {
   fs.readdir(dirname, (err, filenames) => {
@@ -91,7 +77,7 @@ const readFiles = (dirname, onFileContent, onError) => new Promise((resolve, rej
     // filter out files that don't have the right extension
     if(extensions.length > 0) 
       files = files.filter(file => extensions.includes(path.extname(file)));
-
+    
     let promises = files.map(name => new Promise((resolve, reject) => {
       fs.readFile(`${dirname}/${name}`, 'utf-8', (err, content) => {
         if (err) {
@@ -102,6 +88,12 @@ const readFiles = (dirname, onFileContent, onError) => new Promise((resolve, rej
         resolve();
       });
     }));
+
+    if (!recursive) {
+      Promise.all(promises).then(resolve).catch(reject);
+      return;
+    }
+
     // filter out node_modules - perhaps we'll need to filter out other things
     directories = directories.filter(dir => !ignores.reduce((acc, ignore) => acc ? acc : dir.indexOf(ignore) !== -1, false));
     
@@ -113,9 +105,10 @@ const readFiles = (dirname, onFileContent, onError) => new Promise((resolve, rej
 
 readFiles(directory, (filename, content) => data.push(content), (err) => { throw err })
 .then(() => {
-  words = words.map(WORD => WORD.toLowerCase());
-  const foundWords =
-    words.filter(text => data.reduce((acc, content) => acc ? acc : content.toLowerCase().includes(text), false))
+  const filterFn = caseSensitive
+    ? text => data.reduce((acc, content) => acc ? acc : content.toLowerCase().includes(text), false)
+    : text => data.reduce((acc, content) => acc ? acc : content.includes(text), false)
+  const foundWords = words.filter(filterFn)
   const notFoundWords = words.filter(word => !foundWords.includes(word.toLowerCase()))
   console.log(`found instances of:`, foundWords);
   console.log(`didn't find instances of:`, notFoundWords);
